b5d2d72443a368c86ed1aa5e0070e71aafbcf651
hhp227study <83169005+hhp227study@users.noreply.github.com>
2024-09-24 21:14:22 +0900

    Update B7568_huipyo.java

--- week2/B7568_huipyo.java
+++ week2/B7568_huipyo.java
@@ -11,17 +11,15 @@ public class Main {
         Arrays.sort(arrays, (o1, o2) -> o1[1] != o2[1] ? o2[1] - o1[1] : o2[2] - o1[2]);
 
         // 등수 나열하기
-        if (arrays[0][3] == 0) {
-            arrays[0][3] = 1;
-        }
-        for (int i = 1; i < arrays.length; i++) {
-            if (arrays[i - 1][1] > arrays[i][1]) {
-                if (arrays[i - 1][2] < arrays[i][2]) {
-                    arrays[i][3] = arrays[i - 1][3];
-                } else {
-                    arrays[i][3] = i + 1;
+        for (int i = 0; i < arrays.length; i++) {
+            int count = 0;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (arrays[i][1] < arrays[j][1] && arrays[i][2] < arrays[j][2]) {
+                    count++;
                 }
             }
+            arrays[i][3] = count + 1;
         }
         Arrays.sort(arrays, Comparator.comparingInt(o -> o[0]));
         for (int i = 0; i < arrays.length; i++) {
